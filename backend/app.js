4c24d239d892078b982328d20d8cf91b64766452
YannZEUTSOP <yannzeutsop7@gmail.com>
2024-11-29 22:54:30 +0100

    adding a feature as comment in the code

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const taskRoutes = require("./routes/tasks");
 const cors = require('cors');
-
+/*commit my-feature*/
 const app = express();
 
 app.use(cors());
